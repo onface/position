0b660bb044e768697d6a7fef3747738418d68e3f
Nimo <nimo.jser@gmail.com>
2018-05-02 17:07:32 +0800

    0.2.0

---
+++ doc/basic.demo.js
@@ -0,0 +1,35 @@
+var position = require('face-position')
+// 
+// position({
+//     el: document.getElementById('el1'),
+//     target: document.getElementById('target1')
+// })
+//
+//
+//
+//
+// document.getElementById('set').addEventListener('submit', function (e) {
+//     e.preventDefault()
+//     var json = document.getElementById('options').value
+//     var options = JSON.parse(json)
+//     options.el = document.getElementById('el1')
+//     options.target = document.getElementById('target1')
+//     options.useCssTransform = true
+//     options.onAlign = function onAlign (positionition, el, target, offsetParent) {
+//         console.log(positionition)
+//         // positionition.left = positionition.left + 10
+//         return positionition
+//     }
+//     position(options)
+// })
+
+var box = document.createElement('div')
+box.style.width = '20px'
+box.style.height = '20px'
+box.style.backgroundColor = 'orange'
+document.body.appendChild(box)
+
+position({
+    el: box,
+    target: document.getElementById('wrap1')
+})
